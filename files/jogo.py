56f0117418aa5dffc622a60e01cabf6dfe6e31dc
henricaioo <12201000103@muz.ifsuldeminas.edu.br>
2026-05-18 16:49:35 -0300

    ajustes ia

--- files/jogo.py
+++ files/jogo.py
@@ -12,14 +12,17 @@ pygame.init()
 w = 1280
 h = 720
 
-perda = 0
+perda = False
+energia = True
+som = True
 
 mapadino = {
     'palco': ['lounge', 'manutencao'],
     'lounge': [ 'arcade' ],
     'manutencao': ['lounge'],
     'arcade':['portaa'],
-    'portaa': ['escritorio']
+    'portaa': ['escritorio'],
+    'escritorio': []
 }
 
 mapagata = {
@@ -27,7 +30,8 @@ mapagata = {
     'lounge': ['som', 'cozinha'],
     'som':['lounge'],
     'cozinha': ['portab'],
-    'portab': ['escritorio']
+    'portab': ['escritorio'],
+    'escritorio': []
 }
 
 mapajoker = {
@@ -35,7 +39,8 @@ mapajoker = {
     'cozinha': ['portab'],
     'arcade':['portaa'],
     'portaa': ['escritorio'],
-    'portab': ['escritorio']
+    'portab': ['escritorio'],
+    'escritorio': []
 }
 
 mapakangoo = {
@@ -45,7 +50,8 @@ mapakangoo = {
     'cozinha': ['portab'],
     'arcade':['portaa'],
     'portaa': ['escritorio'],
-    'portab': ['escritorio']
+    'portab': ['escritorio'],
+    'escritorio': []
 }
 
 mapabat = {
@@ -54,7 +60,7 @@ mapabat = {
     'manutencao': ['lounge'],
     'cozinha': ['armazem', 'portab'],
     'armazem': ['gerador', 'cozinha'],
-    'gerador':['armazem'],
+    'gerador':[],
     'portab': ['escritorio'],
     'escritorio': []
 }
@@ -77,6 +83,7 @@ class Animatronic(pygame.sprite.Sprite):
         self.mapa = mapa
         self.inicial = pos
         self.delay = delay
+        self.vezes = 1
         self.update(0, 0, 0)
 
 
@@ -85,10 +92,47 @@ class Animatronic(pygame.sprite.Sprite):
         # verifica se o delay do personagem já acabou e se o jogo não foi perdido
         if tempo >= self.delay and perda == 0:
             # verifica se o personagem está em alguma porta e se 5 seg se passaram
+            if self.pos == 'gerador' and (tempo - self.moved) >= 5000:
+                global energia
+                energia = False
+                self.vezes += 1
+                self.pos = self.inicial
+
+            if self.pos == 'som' and (tempo - self.moved) >= 5000:
+                global som
+                som = False
+                self.vezes += 1
+                self.pos = self.inicial
+
+            if self.pos == 'lounge' and 'som' in self.mapa and (tempo - self.moved) >= 5000:
+                itens = self.mapa['lounge']
+                pesos = [1, (1 * self.vezes)]
+                self.cont = tempo
+                num = Rand.randrange(1, 20)
+                # se o numero gerado for menor ou igual a dificuldade do personagem, ele se move
+                if num <= self.dif:
+                    self.pos = Rand.choices(itens, pesos)
+                    print("pos: ", self.pos)
+                    self.moved = tempo
+                    print(self.moved)
+
+            if self.pos == 'cozinha' and 'gerador' in self.mapa and (tempo - self.moved) >= 5000:
+                itens = self.mapa['cozinha']
+                pesos = [1, (1 * self.vezes)]
+                self.cont = tempo
+                num = Rand.randrange(1, 20)
+                # se o numero gerado for menor ou igual a dificuldade do personagem, ele se move
+                if num <= self.dif:
+                    decidido = Rand.choices(itens, pesos)
+                    self.pos = decidido[0]
+                    print("pos: ", self.pos)
+                    self.moved = tempo
+                    print(self.moved)
+
             if self.pos == 'portaa' and (tempo - self.moved) >= 5000 :
                 # caso afirmativo, verifica se a porta está aberta, se sim, ataca
                 if portaa == 0:
-                    perda = 1
+                    perda = True
                     print(self.pos)
                     print('perdeu')
                 else:
@@ -117,11 +161,6 @@ class Animatronic(pygame.sprite.Sprite):
                     self.moved = tempo
                     print(self.moved)
 
-class Cam(pygame.sprite.Sprite):
-    def __init__(self, x, y, nome):
-        self.x = x
-        self.y = y
-        self.nome = nome
               
         
 class Porta(pygame.sprite.Sprite):
@@ -144,18 +183,19 @@ dino = Animatronic(5, 0, 'palco', mapadino, 2000)
 portaa = Porta(235, 365, 10, 20)
 portab = Porta(270, 340, 20, 10)
 cameras = {
-    'manutencao': pygame.Rect(85, 0, 120, 50),
-    'palco': pygame.Rect(135, 60, 50, 20),
-    'som': pygame.Rect(25, 60, 50, 50),
-    'lounge': pygame.Rect(85, 90, 150, 120),
-    'arcade': pygame.Rect(0, 180, 75, 150),
-    'cozinha': pygame.Rect(85, 220, 70, 50),
-    'armazem': pygame.Rect(165, 220, 70, 50),
-    'gerador': pygame.Rect(165, 280, 70, 50)
+    'manutencao': pygame.Rect(85+900, 0+300, 120, 50),
+    'palco': pygame.Rect(135+900, 60+300, 50, 20),
+    'som': pygame.Rect(25+900, 60+300, 50, 50),
+    'lounge': pygame.Rect(85+900, 90+300, 150, 120),
+    'arcade': pygame.Rect(0+900, 180+300, 75, 150),
+    'cozinha': pygame.Rect(85+900, 220+300, 70, 50),
+    'armazem': pygame.Rect(165+900, 220+300, 70, 50),
+    'gerador': pygame.Rect(165+900, 280+300, 70, 50)
 }
 cam = 'palco'
 
 while True:
+    tela.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -166,9 +206,6 @@ while True:
             for nome_cam, retangulo in cameras.items():
                 if retangulo.collidepoint(mouse_pos):
                     cam = nome_cam
-                    print('muda de camera')
-
-    tela.fill((0,0,0))
 
     for nome_cam, retangulo in cameras.items():
         # Desenha o botão: cor clara se selecionada, escura se não
@@ -178,7 +215,7 @@ while True:
         pygame.draw.rect(tela, (255, 255, 255), retangulo, 2)
 
     # desenho base do mapa na tela
-    pygame.draw.rect(tela, (200, 200, 200), (85, 280, 70, 50))
+    pygame.draw.rect(tela, (200, 200, 200), (85+900, 280+300, 70, 50))
     
 
     tempo = pygame.time.get_ticks()
@@ -186,10 +223,10 @@ while True:
     portab.doorcontrol()
     morcego.update(tempo, portaa.state, portab.state)
 
-    if dino.pos == cam:
+    if dino.pos == cam and energia == True:
         pygame.draw.rect(tela, (255, 255, 0), (600, 350, 20, 20))
 
-    if morcego.pos == cam:
+    if morcego.pos == cam and energia == True:
         pygame.draw.rect(tela, (255, 0, 255), (630, 350, 20, 20))
 
 
